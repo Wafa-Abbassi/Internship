b834cec3ab14a7b0f7561e8eef02e5efe6614423
Wafa Abbassi <abbassiiwafa04@gmail.com>
2018-09-19 10:26:27 +0100

    Changes done

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,16 @@
-class Employee < ApplicationRecord
+class Employee < ActiveRecord::Base
+  validates_presence_of :NIC
+
+  #def be_valid(*args)
+    #BeValid.new(*args)
+  #end
+
+  #def not_be_valid(*args)
+    #BeValid.new(*args)
+  #end
+
+  #def new
+    #something new
+  #end
+  
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,19 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+
+  #-------BASIC VALIDATIONS SPECCED OUT-----
+  #it "is valid with valid attributes"
+  #it "is not valid without a NIC"
+  #it "is not valid without a firstName"
+  #it "is not valid without a lastName"
+  #it "is not valid without a login"
+  #it "is not valid without a pwd"
+
+
   subject { described_class.new }
   subject { described_class.new(pwd: "some_password", login: "john@doe.com") }
+
+
+
   it "is valid with valid attributes" do
-    subject.firstName = "Anything"
+
+    #-------THE GREEN STEP-----------
+    #expect(Employee.new(NIC: 'Anything')).to be_valid
+
+    #-------REFACTORED SPEC----------
+    subject.NIC = "Anything"
+    expect(subject).to be_valid
+
   end
 
-  it "is not valid without a NIC"
+  it "is not valid without a NIC" do
+
+    #-------THE RED STEP-----------
+    #employee = Employee.new(NIC: nil)
+    #expect(employee).to_not be_valid
+
+    #-------REFACTORED SPEC--------
+    expect(subject).to_not be_valid
+
+  end
+
   it "is not valid without a firstName"
-  it "is not valid without a login" do 
-    subject.login = nil
-  end
-  it "is not valid without a pwd" do 
-    subject.pwd = nil
-  end
-end
+  it "is not valid without a lastName"
 
+  describe "Validations" do
+
+  it "is not valid without an login" do
+    subject.login = nil
+    expect(subject).to_not be_valid
+    end
+
+   it "is not valid without a pwd" do
+     subject.pwd = nil
+     expect(subject).to_not be_valid
+   end
+
+ end
+
+end
